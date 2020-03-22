77b967a4d18fff2a9b35992cdf34821a597c0821
117ami <ssruoz@gmail.com>
2020-03-22 15:31:35 +0800

    [Linux] new blog on read

---
+++ codes/rust1.rs
@@ -0,0 +1,30 @@
+fn print_sum(v: Vec<i32>) {
+    println!("{}", v[0] + v[1]);
+    // v is dropped and deallocated here
+}
+
+fn myprint(v:Vec<i32>){
+    println!("{:?}", v);
+}
+
+fn main() {
+    let mut v = Vec::new(); // creating the resource
+    for i in 1..10 {
+        v.push(i);
+    }
+    // at this point, v is using
+    // no less than 4000 bytes of memory
+    // -------------------
+    // transfer ownership to print_sum:
+    // print_sum(v);
+    // print_sum(v);
+    println!("{:?}", v);
+    println!("{:?}", v);
+    myprint(v);
+    myprint(v);
+    // we no longer own nor anyhow control v
+    // it would be a compile-time error to try to access v here
+    println!("We're done");
+    // no deallocation happening here,
+    // because print_sum is responsible for everything
+}
