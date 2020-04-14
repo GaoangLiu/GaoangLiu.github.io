4e199db6075c9b12c61dcc708571d82e443c4062
117ami <ssruoz@gmail.com>
2020-04-14 11:47:49 +0800

    blog on rust vector was updated

---
+++ rust_test/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Hello, world!");
+    let a1 = [1_i32, 3, 5, 7, 9, 11]; 
+    let a2 = [2_i32, 4, 6];
+    let mut zipped = a1.iter().zip(a2.iter());
+    println!("{:?}", zipped);
+    // println!("{:?}", zipped.next());
+    for z in zipped {
+        println!("{:?}", z);
+    }
+
+    // let v = [1_i32, 3, 3, 1987, 3, 3, 5, 7, 3];
+
+    // let idx = v.iter().rposition(|&x| x == 3).unwrap();
+    // println!("{}", idx);
+
+    // let vmax = *v.iter().max().unwrap();
+    // if vmax == 1987 {
+    //     println!("Haha, the type if i32");
+    // }
+    // println!("Max element {}", vmax);
+
+
+    
+    // let a = [(1_i32, 3_i32), (-1029, 99), (10, 2)];
+    // let xa = *a.iter().max_by_key(|x| x.1).unwrap(); 
+    // println!("Max by key {:?}", xa);
+    
+    // let b = [1_i32, 2, 3];
+    // let xb = *b.iter().max_by(|x, y| x.cmp(&&(7 + **y))).unwrap();
+    // println!("{}", xb);
+}
---
+++ rust_test/target/debug/rust_test.d
@@ -0,0 +1,1 @@
+/Users/alpha/bitwork/117/rust_test/target/debug/rust_test: /Users/alpha/bitwork/117/rust_test/src/main.rs
